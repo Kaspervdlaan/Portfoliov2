ae589d4bbc739057c69df8db9174761c983184d0
Kaspervdlaan <kaspervdlaan@gmail.com>
2022-12-08 12:42:26 +0100

    added popup, deleted contact page, added new form in popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import { ImCross } from "react-icons/im"
 import {animate, motion} from "framer-motion";
 
 import Aside from "./Aside";
+import Footer from "./Footer";
+
 
 function Header({state, setProjects, setAbout, setContact, setNone}) {
     const [menu, setMenu] = useState(false);
@@ -36,18 +38,18 @@ function Header({state, setProjects, setAbout, setContact, setNone}) {
                         About
                     </li>
                 </motion.a>
-                <motion.a animate={{opacity: 100, y: 0}} initial={{opacity: 0, y: -200}} transition={{type: 'spring',delay: .5, duration: .7}} href="#contact">
-                    <li onClick={setContact} className={state === "contact" ? 'px-6 text-orange-color underline underline-offset-2 cursor-pointer' : 'px-6 hover:text-orange-color hover:underline underline-offset-2 cursor-pointer'}>
-                        Contact
-                    </li>
-                </motion.a>
+                {/*<motion.a animate={{opacity: 100, y: 0}} initial={{opacity: 0, y: -200}} transition={{type: 'spring',delay: .5, duration: .7}} href="#contact">*/}
+                {/*    <li onClick={setContact} className={state === "contact" ? 'px-6 text-orange-color underline underline-offset-2 cursor-pointer' : 'px-6 hover:text-orange-color hover:underline underline-offset-2 cursor-pointer'}>*/}
+                {/*        Contact*/}
+                {/*    </li>*/}
+                {/*</motion.a>*/}
             </ul>
 
              <div className={menu ? "flex absolute right-0 top-0 h-screen w-screen md:hidden items-center justify-center bg-dark-color duration-300 bg-opacity-90" : "flex absolute right-[-100vw] top-0 h-screen w-screen md:hidden items-center justify-center bg-dark-color duration-300"}>
                 <ul className="flex flex-col gap-8 text-2xl items-center ">
                     <a href="#projects" onClick={handleMenu}><li className='px-6 '>Projects</li></a>
                     <a href="#about" onClick={handleMenu}><li className='px-6 '>About</li></a>
-                    <a href="#contact" onClick={handleMenu}><li className='px-6 '>Contact</li></a>
+                    {/*<a href="#contact" onClick={handleMenu}><li className='px-6 '>Contact</li></a>*/}
                 </ul>
             </div>
 
@@ -56,6 +58,7 @@ function Header({state, setProjects, setAbout, setContact, setNone}) {
             </button>
 
             <Aside/>
+            <Footer/>
         </header>
     );
 }
